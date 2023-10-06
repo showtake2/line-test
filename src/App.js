5aceef1de3d26d2ae863b19227771272f61fc433
shota <shota_takehara@kronos.jp>
2023-10-06 17:44:47 +0900

    test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      {/* Include the nav component */}
-      {/* Assuming that '../partials/nav.ejs' is a valid React component */}
-      {/* You would import and render it like this */}
-      {/* <Nav /> */}
       
       <div className="jumbotron text-center">
         <div className="container">
@@ -19,9 +15,4 @@ const App = () => {
 }
 
 export default App;
-このコードでは、ReactのコンポーネントであるAppを作成し、<div>や<a>などのHTML要素をReact要素に変換しました。また、<% include ../partials/nav.ejs %>というEJSテンプレートのインクルードは、Reactではコンポーネントをインポートしてレンダリングすることに置き換えられます。したがって、../partials/nav.ejsを適切なReactコンポーネントに置き換える必要があります。
-
-
-
-
 
